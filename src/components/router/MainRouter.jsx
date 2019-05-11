addf53734e965b4102b17e564212e976c09b757c
AntonPochernikov <36863354+AntonPochernikov@users.noreply.github.com>
2019-05-11 17:25:56 +0300

    redirect when there`s no matching route

--- src/components/router/MainRouter.jsx
+++ src/components/router/MainRouter.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 import './MainRouter.css';
 import PageLoader from 'components/common/PageLoader.jsx';
 import { Tetris, HomePage } from './LazyRoutes.js';
@@ -10,13 +15,13 @@ const renderHome = () => <HomePage />;
 
 const MainRouter = () => (
   <Router>
-    <>
-      <React.Suspense fallback={<PageLoader />}>
-        <Route exact path='/' render={renderRoot} />
+    <React.Suspense fallback={<PageLoader />}>
+      <Switch>
         <Route path='/tetris' render={renderTetris} />
-        <Route path='/home' component={renderHome} />
-      </React.Suspense>
-    </>
+        <Route path='/home' render={renderHome} />
+        <Route render={renderRoot} />
+      </Switch>
+    </React.Suspense>
   </Router>
 );
 
