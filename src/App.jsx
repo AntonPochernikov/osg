30ac50cd846ee717a14f213dbd846efd9daf294b
AntonPochernikov <36863354+AntonPochernikov@users.noreply.github.com>
2019-05-30 21:23:56 +0300

    add style theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+import theme from 'constants/theme.js';
 import store from './store/store.js';
 import './App.css';
 import MainRouter from './components/router/MainRouter.jsx';
 
 render(
   <Provider store={store}>
-    <ThemeProvider theme={{}}>
+    <ThemeProvider theme={theme}>
       <MainRouter />
     </ThemeProvider>
   </Provider>,
---
+++ src/constants/theme.js
@@ -0,0 +1,50 @@
+export default {
+  colorAlertLighten: 'rgb(244, 197, 200)',
+  colorAlertLight: 'rgb(238, 164, 168)',
+  colorAlert: 'rgb(232, 131, 136)',
+
+  colorInactive: '#b9c0cb',
+
+  colorInfoLightest: 'rgb(250, 253, 253)',
+  colorInfoLighten: 'rgb(191, 229, 234)',
+  colorInfoLight: 'rgb(146, 212, 219)',
+  colorInfo: 'rgb(102, 194, 205)',
+  colorInfoDark: 'rgb(61, 174, 187)',
+  colorInfoDarken: 'rgb(47, 132, 143)',
+  colorInfoDarkest: 'rgb(32, 91, 98)',
+
+  colorYellowLighten: '#ffeac3',
+  colorYellowLight: '#ffda97',
+  colorYellow: '#ffcb6b',
+  colorYellowDark: 'rgb(215, 199, 111)',
+  colorYellowDarken: 'rgb(206, 187, 80)',
+  colorYellowDarkest: 'rgb(193, 172, 54)',
+
+  colorOrangeLightest: 'rgb(244, 229, 214)',
+  colorOrangeLighten: 'rgb(235, 209, 183)',
+  colorOrangeLight: 'rgb(227, 190, 152)',
+  colorOrange: 'rgb(219, 171, 121)',
+  colorOrangeDark: 'rgb(219, 171, 121)',
+  colorOrangeDarken: 'rgb(210, 151, 90)',
+  colorOrangeDarkest: 'rgb(202, 132, 59)',
+
+  colorSuccess: 'rgb(157, 242, 157)',
+  colorSuccessDark: 'rgb(122, 237, 122)',
+  colorSuccessDarken: 'rgb(88, 233, 88)',
+  colorSuccessDarkest: 'rgb(53, 228, 53)',
+
+  colorNeutralLightest: 'rgb(207, 212, 219)',
+  colorNeutralLighten: 'rgb(174, 182, 194)',
+  colorNeutralLight: 'rgb(140, 151, 168)',
+  colorNeutral: 'rgb(91, 102, 120)',
+  colorNeutralDark: 'rgb(65, 73, 87)',
+  colorNeutralDarken: 'rgb(38, 50, 56)',
+  colorNeutralDarkest: 'rgb(15, 17, 19)',
+
+  colorMain: 'rgb(101, 136, 152)',
+  colorMainDark: 'rgb(85, 115, 128)',
+  colorMainDarken: 'rgb(70, 94, 105)',
+  colorMainDarkest: 'rgb(54, 73, 81)',
+
+  widthContent: '768px',
+};
