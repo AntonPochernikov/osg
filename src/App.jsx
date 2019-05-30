5a6b07b1aedf5889ac8c7f1f4c08e60d910fc512
AntonPochernikov <36863354+AntonPochernikov@users.noreply.github.com>
2019-05-30 21:15:05 +0300

    provide style theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import store from './store/store.js';
 import './App.css';
 import MainRouter from './components/router/MainRouter.jsx';
 
 render(
   <Provider store={store}>
-    <MainRouter />
+    <ThemeProvider theme={{}}>
+      <MainRouter />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root'),
 );
