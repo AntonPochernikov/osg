d07cf7e608671158b8175b538b566576d8380eb6
AntonPochernikov <36863354+AntonPochernikov@users.noreply.github.com>
2019-06-03 21:33:25 +0300

    remove styled-components - too bad performance with cell states(

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
-import theme from 'constants/theme.js';
 import store from './store/store.js';
 import './App.css';
 import MainRouter from './components/router/MainRouter.jsx';
 
 render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <MainRouter />
-    </ThemeProvider>
+    <MainRouter />
   </Provider>,
   document.getElementById('root'),
 );
