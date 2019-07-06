ce548c9f672b4092f3523cf4b13c6181b0520159
AntonPochernikov <akpochernikov@gmail.com>
2019-07-06 14:17:47 +0300

    Updates

---
+++ precache-manifest.acf30c3bf6d549386bd478ec0c0affa9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8109c06d8e733b88e1d52516a01c34",
+    "url": "/osg/index.html"
+  },
+  {
+    "revision": "fffd99fa17522fc9bfa9",
+    "url": "/osg/static/css/home.8cb02bc2.chunk.css"
+  },
+  {
+    "revision": "cc190dad049a30b8e559",
+    "url": "/osg/static/css/main.9e12d2f0.chunk.css"
+  },
+  {
+    "revision": "c8f05e01dacc0044e9b8",
+    "url": "/osg/static/css/tetris.da3effa2.chunk.css"
+  },
+  {
+    "revision": "997a2e70d2de4fc984cf",
+    "url": "/osg/static/js/5.faca1e04.chunk.js"
+  },
+  {
+    "revision": "8c6c0f30b3f9bfb9aab1",
+    "url": "/osg/static/js/6.8248c8db.chunk.js"
+  },
+  {
+    "revision": "fffd99fa17522fc9bfa9",
+    "url": "/osg/static/js/home.6b3d08f5.chunk.js"
+  },
+  {
+    "revision": "cc190dad049a30b8e559",
+    "url": "/osg/static/js/main.8ced75e7.chunk.js"
+  },
+  {
+    "revision": "67565e026cf716b7557d",
+    "url": "/osg/static/js/runtime~main.4550706d.js"
+  },
+  {
+    "revision": "318fc4928b6c6968b4f1",
+    "url": "/osg/static/js/tetris-reducer.33bc2bd7.chunk.js"
+  },
+  {
+    "revision": "c8f05e01dacc0044e9b8",
+    "url": "/osg/static/js/tetris.6636f544.chunk.js"
+  }
+]);
